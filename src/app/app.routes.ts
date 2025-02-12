3b3de0b9722e403147f5ce3711e24fb57374833d
Lev <lev.bereza@gmail.com>
2025-02-12 20:43:20 +0200

    Added signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {OrderListComponent} from './components/order/order-list/order-list.compo
 import {HomeComponent} from './components/home/home.component';
 import {ProductListComponent} from './components/product/product-list/product-list.component';
 import {ProductDetailComponent} from './components/product/product-detail/product-detail.component';
+import {SignupComponent} from './components/auth/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'orders', component: OrderListComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'products/:id', component: ProductDetailComponent },
 ];
---
+++ src/app/components/auth/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  imports: [],
+  templateUrl: './signup.component.html',
+  standalone: true,
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
