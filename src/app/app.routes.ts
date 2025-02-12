d19e89ff6c58e13fdd953eeca29cb973cc062e03
Lev <lev.bereza@gmail.com>
2025-02-12 20:46:58 +0200

    Added login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {HomeComponent} from './components/home/home.component';
 import {ProductListComponent} from './components/product/product-list/product-list.component';
 import {ProductDetailComponent} from './components/product/product-detail/product-detail.component';
 import {SignupComponent} from './components/auth/signup/signup.component';
+import {LoginComponent} from './components/auth/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'orders', component: OrderListComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'products/:id', component: ProductDetailComponent },
 ];
---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  standalone: true,
+  styleUrl: '../signup/signup.component.css'
+})
+export class LoginComponent {
+
+}
