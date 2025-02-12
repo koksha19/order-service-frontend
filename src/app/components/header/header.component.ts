55b1bb03264b99899a19a5cd732755a2761fbe06
Lev <lev.bereza@gmail.com>
2025-02-12 18:45:30 +0200

    Identified active routes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {NgOptimizedImage} from '@angular/common';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, NgOptimizedImage, RouterLink],
+  imports: [MatToolbarModule, NgOptimizedImage, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   standalone: true,
   styleUrl: './header.component.css'
