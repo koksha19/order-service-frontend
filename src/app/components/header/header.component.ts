af8898058dff3a3e14e216594504cdea938323fb
Lev <lev.bereza@gmail.com>
2025-02-13 11:12:01 +0200

    Made header responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {NgOptimizedImage} from '@angular/common';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, NgOptimizedImage, RouterLink, RouterLinkActive],
+  imports: [MatToolbarModule, NgOptimizedImage, RouterLink, RouterLinkActive, MatMenuTrigger, MatIcon, MatMenu, MatMenuItem, MatIconButton],
   templateUrl: './header.component.html',
   standalone: true,
   styleUrl: './header.component.css'
