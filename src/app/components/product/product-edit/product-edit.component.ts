65c633ecf0e19d1998d3606086f743374bab9684
Lev <lev.bereza@gmail.com>
2025-02-13 14:38:59 +0200

    Choosing possible delivery options

--- src/app/components/product/product-edit/product-edit.component.ts
+++ src/app/components/product/product-edit/product-edit.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import { Delivery } from '../../../models/delivery.model';
+import {MatCheckbox} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-product-edit',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    MatCheckbox
+  ],
   templateUrl: './product-edit.component.html',
   standalone: true,
   styleUrl: '../../auth/signup/signup.component.css',
 })
 export class ProductEditComponent {
+  readonly delivery = signal<Delivery>({
+    name: 'Select all',
+    checked: false,
+    options: [
+      {name: 'Standard', price: 50, checked: false},
+      {name: 'Express', price: 70, checked: false},
+      {name: 'Overnight', price: 100, checked: false},
+    ],
+  });
 
+  readonly partiallyComplete = computed(() => {
+    const delivery = this.delivery();
+    if (!delivery.options) {
+      return false;
+    }
+    return delivery.options.some(d => d.checked) && !delivery.options.every(d => d.checked);
+  });
+
+  update(checked: boolean, index?: number) {
+    this.delivery.update(delivery => {
+      if (index === undefined) {
+        delivery.checked = checked;
+        delivery.options?.forEach(d => (d.checked = checked));
+      } else {
+        delivery.options![index].checked = checked;
+        delivery.checked = delivery.options?.every(t => t.checked) ?? true;
+      }
+      return {...delivery};
+    });
+  }
 }
