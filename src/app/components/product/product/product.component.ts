9332910dc7b8626f25cbbb6b487ac251e8eb859d
Lev <lev.bereza@gmail.com>
2025-02-17 10:36:58 +0200

    Added edit and delete buttons for admin products

--- src/app/components/product/product/product.component.ts
+++ src/app/components/product/product/product.component.ts
@@ -1,13 +1,24 @@
-import {Component, input} from '@angular/core';
-import {Product} from '../../../models/product.model';
+import { Component, input } from '@angular/core';
+import { Product } from '../../../models/product.model';
+import { NgClass, NgIf } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [NgIf, NgClass, MatButton],
   templateUrl: './product.component.html',
   standalone: true,
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
   product = input.required<Product>();
+
+  public isAdminRoute = false;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.url.subscribe((segments) => {
+      this.isAdminRoute = segments.some((segment) => segment.path === 'admin');
+    });
+  }
 }
